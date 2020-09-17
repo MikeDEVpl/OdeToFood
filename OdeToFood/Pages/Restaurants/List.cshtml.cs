fb4ebafd85843a7e2e2dca0844431866dea7ccce
MikeDevPl <46839276+MikeDEVpl@users.noreply.github.com>
2020-09-17 13:04:36 +0200

    in Memory repository for restaurants and html poc

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
+        public string Message { get; set; }
+        private readonly IRestaurantData restaurantData;
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
         public void OnGet()
         {
-
-        }
+            Message = config["Message"]; // pobranie wartosci z appsettings.json
+            //Message = "Hello World!";
+            Restaurants = restaurantData.GetAll();
+         }
     }
 }
